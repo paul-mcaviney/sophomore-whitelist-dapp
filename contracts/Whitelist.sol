4123f6c675d9d38446ad6d7dbef1cca0188d92fd
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2022-05-27 07:54:46 -0600

    Finish whitelist contract

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -10,5 +10,28 @@ contract Whitelist {
     // If an address is whitelisted, set to true, false by default
     mapping(address => bool) public whitelistedAddresses;
 
-    
+    // Keep track of number of whitelisted address
+    uint8 public numAddressesWhitelisted;
+
+    // Initialize contract with a max number of whitelisted addresses
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /*
+        Add the address of the sender to the whitelist
+    */
+    function addAddressToWhitelist() public {
+        // Check if user has already been whitelisted
+        require(!whitelistedAddresses[msg.sender], "addAddressToWhitelist Error: Sender has already been whitelisted");
+
+        // Check if maxWhitelistedAddresses has been reached
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "addAddressToWhitelist Error: Max number of whitelisted addresses already reached");
+
+        // Add the address of the message sender to whitelistedAddresses
+        whitelistedAddresses[msg.sender] = true;
+
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
+    }
 }
